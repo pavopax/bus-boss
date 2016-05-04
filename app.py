97d9456ea8f02463f96a01bca88f1c491854240b
paul <pavopax@gmail.com>
2016-05-04 19:24:55 -0400

    trying heatmap with custom points (leaflet.js)

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template, request, redirect
 
+import fetching                 # my fetching.py file
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/leaflet')
+def leaflet():
+
+    # get some data:
+    nyc = nyc_current()
+
+    plotdf = nyc[['MonitoredVehicleJourney_PublishedLineName',
+                  'MonitoredVehicleJourney_VehicleLocation_Latitude',
+                  'MonitoredVehicleJourney_VehicleLocation_Longitude']]
+
+    # add this for plot (may not be using yet)
+    plotdf = plotdf.assign(intensity=0.2);
+    x = list(plotdf.itertuples())
+
+    return render_template('leaflet.html')
+
+@app.route('/live_example')
+def live_example():
+    return render_template('live_example.html')
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
---
+++ fetching.py
@@ -0,0 +1,24 @@
+import json
+import pandas as pd
+import requests
+
+MTA_API_KEY = "8c9aa77a-eb79-41be-a77c-909d8fc9ffc9"
+MTA_API_BASE = "http://bustime.mta.info/api/siri/vehicle-monitoring.json"
+
+
+def _flatten_dict(root_key, nested_dict, flattened_dict):
+    for key, value in nested_dict.iteritems():
+        next_key = root_key + "_" + key if root_key != "" else key
+        if isinstance(value, dict):
+            _flatten_dict(next_key, value, flattened_dict)
+        else:
+            flattened_dict[next_key] = value
+    return flattened_dict
+    
+
+params = {"key": MTA_API_KEY}
+#This is useful for the live MTA Data
+def nyc_current():
+    resp = requests.get(MTA_API_BASE, params=params).json()
+    info = resp['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+    return pd.DataFrame([_flatten_dict('', i, {}) for i in info])
